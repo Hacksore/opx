bb537ba6439faa9e65fa59477ff121e666f0a6cb
Sean Boult <996134+Hacksore@users.noreply.github.com>
2023-03-21 12:20:42 -0500

    Update

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
 #![allow(clippy::needless_return)]
 
-mod util;
 mod config;
+mod util;
 
-use config::OpxConfig;
 use anyhow::Result;
+use config::OpxConfig;
 
 use crate::util::{get_env_files, run_op_command};
 use std::env;
@@ -14,10 +14,10 @@ fn main() -> Result<()> {
   let cli_args = env::args().skip(1).collect::<Vec<String>>();
 
   // read config from the local director if possible
-  let config = OpxConfig::new()?; 
+  let config = OpxConfig::new()?;
   let package_manager = config.get_package_manager();
 
-  // default command for now is start but this should be configurable 
+  // default command for now is start but this should be configurable
   let op_args = match cli_args {
     args if args.is_empty() => vec!["start".to_string()],
     args => args,
