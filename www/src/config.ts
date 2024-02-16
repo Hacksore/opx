d07b536d43f25ee94f20ba8aa4bf170328dc366c
Sean Boult <996134+Hacksore@users.noreply.github.com>
2024-02-16 09:32:44 -0600

    make small

--- www/src/config.ts
+++ www/src/config.ts
@@ -1,3 +1,3 @@
 export const SITE_TITLE = "opx - No plaintext secrets at rest";
-export const SITE_DESCRIPTION = "Run NodeJS apps with Secrets from 1Password";
+export const SITE_DESCRIPTION = "Run NodeJS apps with secrets from 1Password";
 export const SITE_IMAGE = "/og.png";
