9cd08088a4e24cd5c20dda616c85ee8b38b27f4c
Sean Boult <996134+Hacksore@users.noreply.github.com>
2024-02-16 08:04:29 -0600

    Eslint and format

---
+++ www/.eslintrc.cjs
@@ -0,0 +1,14 @@
+module.exports = {
+  extends: [require.resolve("lint/node")],
+  overrides: [
+    {
+      files: ["*.astro"],
+      parser: "astro-eslint-parser",
+      parserOptions: {
+        parser: "@typescript-eslint/parser",
+        extraFileExtensions: [".astro"],
+      },
+      rules: {},
+    },
+  ],
+};
