3bbeb9f7b8a16f23016afd23a584f04c9f60ebee
Sean Boult <996134+Hacksore@users.noreply.github.com>
2024-02-16 15:09:38 -0600

    Format

--- www/src/config.ts
+++ www/src/config.ts
@@ -1,3 +1,4 @@
 export const SITE_TITLE = "opx - No Plaintext Secrets at Rest";
-export const SITE_DESCRIPTION = "Run NodeJS apps with secrets from 1Password CLI";
+export const SITE_DESCRIPTION =
+  "Run NodeJS apps with secrets from 1Password CLI";
 export const SITE_IMAGE = "/og.png";
