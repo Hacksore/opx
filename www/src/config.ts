6761e09043fbdc0883fee9ac2e90a8fc59c7c1cb
Sean Boult <996134+Hacksore@users.noreply.github.com>
2024-02-16 23:02:52 -0600

    Make caps

--- www/src/config.ts
+++ www/src/config.ts
@@ -1,4 +1,4 @@
-export const SITE_TITLE = "opx | A 1Password companion CLI";
+export const SITE_TITLE = "OPX | A 1Password companion CLI";
 export const SITE_DESCRIPTION =
   "The most secure way to run NodeJS apps locally!";
 export const SITE_IMAGE = "/og.png";
