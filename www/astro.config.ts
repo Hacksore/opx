9657f082ef41062b48ee4178d33fab1bb56baae6
Sean Boult <996134+Hacksore@users.noreply.github.com>
2026-07-19 13:32:51 -0500

    Upgrade astro

--- www/astro.config.ts
+++ www/astro.config.ts
@@ -1,16 +1,15 @@
 import { defineConfig } from "astro/config";
 import vercel from "@astrojs/vercel";
-import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
-import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
 import compress from "astro-compress";
 
-const integrations = [sitemap(), react(), tailwind(), mdx(), compress()];
+const integrations = [sitemap(), mdx(), compress()];
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://tryopx.com",
+  output: "static",
   trailingSlash: "never",
   integrations,
   adapter: vercel({
